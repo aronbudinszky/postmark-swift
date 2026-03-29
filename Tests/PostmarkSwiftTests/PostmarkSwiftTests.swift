0d88982bcdeafa12d253ab937f08afd16d5cf64a
Aron Budinszky <aron@budinszky.me>
2026-03-29 14:18:58 +0200

    Convert to Swift testing

--- Tests/PostmarkSwiftTests/PostmarkSwiftTests.swift
+++ Tests/PostmarkSwiftTests/PostmarkSwiftTests.swift
@@ -1,11 +1,7 @@
-import XCTest
+import Testing
 @testable import PostmarkSwift
 
-final class PostmarkSwiftTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(PostmarkSwift().text, "Hello, World!")
+@Suite struct PostmarkSwiftTests {
+    @Test func example() {
     }
 }
